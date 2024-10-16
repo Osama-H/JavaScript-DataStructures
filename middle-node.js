34110ce3cd56bee51506e6222e5280bf3408a7c2
Osama-H <osama_29_@hotmail.com>
2024-10-16 15:13:58 +0300

    Solve Middle Node on leetCode

---
+++ middle-node.js
@@ -0,0 +1,13 @@
+// Solve getting middleNode on leetCode
+
+const middleNode = (head) => {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+};
